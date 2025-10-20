bd6c5601fa59d7bfc7dc9bd07198c29b68944851
BazookaB3n <62162445+BazookaB3n@users.noreply.github.com>
2025-10-20 08:46:52 -0400

    Update Search.java
    updated search class so it works and returns 10 search results from YouTube

--- Mod/src/main/java/com/retrotechie/MusicJam/Commands/Search.java
+++ Mod/src/main/java/com/retrotechie/MusicJam/Commands/Search.java
@@ -1,30 +1,54 @@
 package com.retrotechie.MusicJam.Commands;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.retrotechie.MusicJam.MainJam;
 import com.retrotechie.MusicJam.Utilities.Command;
 import com.retrotechie.MusicJam.Utilities.CommandRegistry;
 
 public class Search {
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+
 	public static void register(CommandRegistry registry) {
 		Command Search = new Command("search", "Finds a set number of video results for a given prompt", (args) -> {
+			executor.submit(() -> {
 			if(args.length == 0) {
 				return;
 			}
 			String search = args[0].toString();
-			int amount = (int) Double.parseDouble(args[1]);
-			ProcessBuilder pb = new ProcessBuilder(MainJam.pathToYTDLP, "\"ytsearch:\"", search);
-			pb.redirectErrorStream();
+			ProcessBuilder pb = new ProcessBuilder(MainJam.pathToYTDLP, "ytsearch10:" + search, "--get-title");
 			pb.inheritIO();
+			List<String> urls = null;
 			try {
 				Process process = pb.start();
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String url = "https://www.youtube.com/watch?v=" + line.trim();
+                    urls.add(url);
+                process.waitFor();
+    			for(int n = 0; n < urls.size(); n++) {
+    				System.out.println("URL #" + n + ": " + urls.indexOf(n));
+    				}
+                }
+            } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		});
-		
+		});
 		registry.register(Search);
 	}
+	
 }
